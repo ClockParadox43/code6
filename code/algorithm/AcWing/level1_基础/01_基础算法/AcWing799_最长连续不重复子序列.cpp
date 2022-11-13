078d53bcd00a6d1291c624c244fd1226160a1842
zyw <113148080+AbnormalizeZYW@users.noreply.github.com>
2022-11-13 19:38:52 +0800

    创建 AcWing799_最长连续不重复子序列.cpp

---
+++ code/algorithm/AcWing/level1_基础/01_基础算法/AcWing799_最长连续不重复子序列.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int a[N], s[N];
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+
+    for (int i = 0; i < n; ++ i ) scanf("%d", &a[i]);
+    
+    int ans = 0;
+    // i 枚举往右扩
+    for (int i = 0, j = 0; i < n; ++ i ) 
+    {
+        s[a[i]] ++ ;
+        // 一旦大于 1 就将之前记录去掉
+        while (s[a[i]] > 1) 
+        {
+            s[a[j]] -- ;
+            ++ j;
+        }
+        ans = ans > (i  - j  + 1) ? ans : (i - j + 1);
+    }
+    
+    printf("%d", ans);
+    
+    return 0;
+}
