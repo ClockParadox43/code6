4b37bcb5053a20b07f0c6d3fc472db31d5deaa90
zyw <113148080+AbnormalizeZYW@users.noreply.github.com>
2022-12-04 19:56:53 +0800

    同余方程

--- code/algorithm/AcWing/level2_提高/05_数学/06_同余/AcWing203_同余方程.cpp
+++ code/algorithm/AcWing/level2_提高/05_数学/06_同余/AcWing203_同余方程.cpp
@@ -1,9 +1,5 @@
 // 复习:
 // 扩展欧几里得算法
-// (a,b) = d
-// 给定一个方程使得 ax + by = d, 求 x,y 这两个系数
-// 
-// 欧几里得算法: (a,b) = (b,a mod b)
 // 
 // 扩展欧几里得原理
 //            (a,b) = d, 
@@ -15,23 +11,57 @@
 // ay + b(x - y⌊a/b⌋) = d
 // x' = y, y' = x - ⌊a/b⌋y
 //
-// 求线性同余方程: ax ≡ b (mod c) 
-//          ax - cy = b
-//           设: y' = -y
-//          ax + cy = b  => 因此可以推出扩展欧几里得算法
+// 因为 a, b 是 (a,b), 所以 ax + by 一定是 (a,b) 的倍数, 那么 ax + by 能凑出的最小数就是 (a,b) 
+// gcd(a, b) 记作 d
+// 想求两个系数 x, y, 有 ax + by = d 
 // 
-// ax ≡ b (mod c) 通常能转换成不定方程的形式:
-//      - ax ≡ b (mod c) <=> ax + cy = b 
-// 不定方程有解条件: 整系数二元不定方向 ax + by = c中的系数 a, b 的最大公约数能整除 c, (a,b)|c
-// 假设已经求出解: x = x₀, y = y₀
+// q: 
+// ax mod c = b => ax = cy + b
 // 
-// q: 求关于 x 的同余方程 ax ≡ 1(mod b) 的最小正整数解
-// 设: ax ≡ 1(mod b)
-// ∵ ax ≡ 1(mod b)
-// ∴ ∃ y∈Z, s.t. ax + by = 1
-// 因此可以通过 扩展欧几里得算法求出 x₀, y₀ 
-// => 所有解: x = x₀ + kb, y = y₀ - ka   
+// 求关于 x 的同余方程 ax ≡ 1(mod b) 的最小正整数解
+// ax ≡ 1(mod b) => ax % b = 1 % b 
+// ∵ 1 % b = 1 
+// ∴ ax % b = 1
+// ∴ ax = by + 1  
+// ∴ ax + (-b)y = 1 => 用扩展欧几里得求出 x₀, y₀
+// 通解: x = x₀ + k(a/d), y = y₀ + k(b/d)
+// 因为余数等于 1, 所以满足 x₀ + k(a/d) 必然不等于 0 
+// ps:如果 x 等于 0, 0 乘任何数都是 0, ax = 0, ax % b 必然没有余数
+
 #include <iostream>
 
 using namespace std;
 
+int exgcd(int a, int b, int &x, int &y)
+{
+    if (!b)
+    {
+        x = 1, y = 0;
+        return a; 
+    }
+
+    int x1, y1;
+    int res = exgcd(b, a % b, x1, y1);
+    x = y1, y = x1 - a / b * y1; 
+    return res;
+}
+
+int main()
+{
+    // ax ≡ 1(mod b) => ax % b = 1 % b 
+    // ∵ 1 % b = 1 
+    // ∴ ax % b = 1
+    // ∴ ax = by + 1
+    // ∴ ax + (-b)y = 1 => 用扩展欧几里得求出 x₀, y₀
+    // 通解: x = x₀ + k(a/d), y = y₀ + k(b/d)
+    int a, b; scanf("%d%d", &a, &b);
+    
+    int x, y;  
+    exgcd(a, b, x, y);
+    
+    // 此时已经求出了 x₀, 我们要求的是 x
+    // 满足等式的最小正整数 = x mod b = (x₀ mod b + b) % b
+    cout << (x % b + b) % b << endl;
+
+    return 0;
+}
