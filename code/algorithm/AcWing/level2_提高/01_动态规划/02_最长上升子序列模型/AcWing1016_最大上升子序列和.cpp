f7e6ab737500cbaea7bd1818f18f7f0939f6123d
zyw <113148080+AbnormalizeZYW@users.noreply.github.com>
2023-01-28 22:49:39 +0800

    最大上升子序列和

--- code/algorithm/AcWing/level2_提高/01_动态规划/02_最长上升子序列模型/AcWing1016_最大上升子序列和.cpp
+++ code/algorithm/AcWing/level2_提高/01_动态规划/02_最长上升子序列模型/AcWing1016_最大上升子序列和.cpp
@@ -1,5 +1,31 @@
-
-
 // 状态表示 f[i]
 //     - 集合: 所有 a[i] 结尾的上升子序列
-//     -   和: 和的最大值
+//     - 属性: 和的最大值
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int MAXN = 1010;
+
+int n;
+int w[MAXN], f[MAXN];
+
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++ i) scanf("%d", &w[i]);
+
+    int res = 0;
+    for (int i = 0; i < n; ++ i)
+    {
+        f[i] = w[i];
+        for (int  j = 0; j < i; ++ j)
+            if (w[i] > w[j])    
+                f[i] = max(f[i], f[j] + w[i]);
+        res = max(res, f[i]);
+    }
+    
+    printf("%d\n", res);
+    return 0;
+}
